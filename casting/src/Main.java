f4e0f014af6a9aabe5a82d2dab7c189fa92b0aea
Williansales404 <willian9542@gmail.com>
2025-05-06 09:46:13 -0300

    casting

--- casting/src/Main.java
+++ casting/src/Main.java
@@ -2,8 +2,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//processo de casting para não perder informação e converter um valor para outro
+		/*
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado);
+		*/
+		
+		//casting
+		double a;
+		int b;
+		a = 5.0;
+		b = (int)a;
+		System.out.println(b);
+		}
 	}
-
-}
